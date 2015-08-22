9be067ffaa63eb276f8e3278de91e6c24d845304
AliceP-R <alice.pottier@free.fr>
2015-08-23 00:21:31 +0200

    2

---
+++ Assets/Move_Player.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Move_Player : MonoBehaviour {
+
+	public int moveSpeed = 10;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetMouseButton (0)) {
+			var targetPos = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+			//targetPos.z = targetPos.y;
+			transform.position = Vector3.MoveTowards (transform.position, targetPos, moveSpeed * Time.deltaTime);
+		}
+	}
+}
