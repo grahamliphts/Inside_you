cd5a0cb4bdc6dbcae6389e409e8c2e5756c1c188
AliceP-R <alice.pottier@free.fr>
2015-08-22 23:08:44 +0200

    Test_Scene added

---
+++ Assets/collider_llert.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class collider_llert : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (other.name == "Capsule (4)")
+			Debug.Log ("coucou");
+	}
+}
