0dfa986a29d3cf73d1f1f0ac3472d62c52849f79
Jonkiye <colinzac@hotmail.fr>
2015-08-23 10:37:51 +0200

    jnh

---
+++ Assets/encounter.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class encounter : MonoBehaviour {
+	public int Pv = 10;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public int getPV()
+	{
+		return Pv;
+	}
+}
